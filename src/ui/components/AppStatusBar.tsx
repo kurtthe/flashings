917a2d53edb8c0258fd927ec6ee0f906526117c8
Vanessa De Pontes <vanedepontes@gmail.com>
2023-07-11 23:00:20 -0400

    fix app status bar component

--- src/ui/components/AppStatusBar.tsx
+++ src/ui/components/AppStatusBar.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import {StatusBar, StatusBarProps} from 'react-native';
+import {StatusBar, StatusBarProps, Platform, View} from 'react-native';
 
 import {useColorModeValue} from '@ui/hooks';
 
 type Props = Omit<StatusBarProps, 'barStyle'> & {
   barStyle?: 'auto' | 'light-content' | 'dark-content';
+  color?: string;
 };
 
-const AppStatusBar = ({barStyle = 'auto', ...rest}: Props) => {
+const AppStatusBar = ({barStyle = 'auto', color = 'white', ...rest}: Props) => {
   const autoStyle = useColorModeValue('dark-content', 'light-content');
-  return (
+  const statusBar = (
     <StatusBar
+      backgroundColor={color}
       barStyle={barStyle === 'auto' ? autoStyle : barStyle}
-      {...rest}
     />
-  );
+  )
+  if (Platform.OS === 'ios') {
+    return (
+      <View style={{ backgroundColor: color, width: '100%', height: 60 }}>{statusBar}</View>
+    )
+  }
+  return statusBar
 };
 
 export default AppStatusBar;
+
