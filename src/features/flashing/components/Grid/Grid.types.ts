d5e0cc2e25b524cb8533196d7a8254d91868faee
Jefferson Solano <solanojefferson@gmail.com>
2023-07-18 21:52:43 -0500

    draw grid

---
+++ src/features/flashing/components/Grid/Grid.types.ts
@@ -0,0 +1,10 @@
+import {Dimensions} from 'react-native';
+
+export const PADDING_BARS = 0;
+export const ALIGN_BARS = 0;
+export const LINE_OFFSET = 0;
+
+export const SIZE_GRID = 50;
+
+export const WIDTH_SCREEN = Dimensions.get('screen').height;
+export const HEIGHT_SCREEN = Dimensions.get('screen').height;
