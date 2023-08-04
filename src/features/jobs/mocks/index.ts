128ebf70e1f212180d628fa7f3f29d30e2ceeec2
Jefferson Solano <solanojefferson@gmail.com>
2023-08-04 18:15:25 -0500

    refactor job list

---
+++ src/features/jobs/mocks/index.ts
@@ -0,0 +1,25 @@
+import { JOB_DETAIL } from '@models';
+
+export const dataJobs: JOB_DETAIL[] = [
+  {
+    id: 1001,
+    name: '6A Pine Cres Ringwood North',
+    address: '6a Pine Cres, Ringwood North VIC, Australia',
+    job_number: '894',
+    client_name: 'Ross Jurey',
+  },
+  {
+    id: 1002,
+    job_number: '1391',
+    name: '20-22 hereford rd Mount Evelyn',
+    address: '20-22 Hereford Rd, Mount Evelyn VIC, Australia',
+    client_name: 'Barry  Biggin',
+  },
+  {
+    id: 1003,
+    job_number: '1392',
+    name: '46 Centre way, Croydon South.',
+    address: '46 Centre Way, Croydon South VIC, Australia',
+    client_name: 'Sharp & Howells',
+  },
+];
---
+++ src/models/jobs.d.ts
@@ -0,0 +1,7 @@
+export type JOB_DETAIL = {
+  id: number;
+  name: string;
+  address: string;
+  job_number: string;
+  client_name: string;
+};
