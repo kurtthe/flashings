45e5bec521c288bc1f6e520518ab81e304c25bf8
Jefferson Solano <solanojefferson@gmail.com>
2023-08-04 18:48:50 -0500

    form to formik

--- src/features/auth/constants/index.ts
+++ src/features/auth/constants/index.ts
@@ -1,4 +1,6 @@
 import * as Yup from 'yup';
+import { isInteger } from 'lodash';
+import { formatPhone } from '@shared/helpers';
 
 export const customValidationMessages = {
   login: {
@@ -11,6 +13,27 @@ export const customValidationMessages = {
     },
   },
 };
+
+const validatePhone = (phone: string | undefined) =>
+  Yup.string()
+    .trim()
+    .test('phone', 'Phone number is invalid', (value: string | undefined) => {
+      if (!value) {
+        return false;
+      }
+
+      const clearValue = value.replace(/[\s()-]/g, '');
+      if (isInteger(Number(clearValue)) && clearValue.length === 10) {
+        const formatedPhone = formatPhone(clearValue);
+        if (formatedPhone.length === 14) return true;
+      }
+      if (value.length === 10) {
+        const formatedPhone = formatPhone(value);
+        if (formatedPhone.length === 14) return true;
+      }
+      return false;
+    })
+    .isValidSync(phone);
 export const forms = {
   login: {
     initialValues: {
@@ -41,6 +64,60 @@ export const forms = {
       password: Yup.string().required(
         customValidationMessages.login.password.required,
       ),
+    }),
+  },
+  createJob: {
+    initialValues: {
+      jobName: '',
+      jobNumber: '',
+      siteAddress: '',
+      fileUpload: '',
+      contactName: '',
+      contactNumber: '',
+      contactEmail: '',
+    },
+    initialErrors: {
+      jobName: '',
+      jobNumber: '',
+      siteAddress: '',
+      fileUpload: '',
+      contactName: '',
+      contactNumber: '',
+      contactEmail: '',
+    },
+    labels: {
+      jobName: 'Job Name',
+      jobNumber: 'Job Number',
+      siteAddress: 'Site Address',
+      fileUpload: 'File Upload',
+      contactName: 'Contact Name',
+      contactNumber: 'Contact Number',
+      contactEmail: 'Contact Email',
+    },
+    placeholders: {
+      jobName: 'Job Name',
+      jobNumber: 'Job Number',
+      siteAddress: 'Site Address',
+      fileUpload: 'File Upload',
+      contactName: 'Contact Name',
+      contactNumber: 'Contact Number',
+      contactEmail: 'Contact Email',
+    },
+    schema: Yup.object({
+      jobName: Yup.string(),
+      jobNumber: Yup.number(),
+      siteAddress: Yup.string(),
+      fileUpload: Yup.string(),
+      contactName: Yup.string(),
+      contactNumber: Yup.string().test('step', 'Phone number', value =>
+        validatePhone(value),
+      ),
+      contactEmail: Yup.string()
+        .email(customValidationMessages.login.email.email)
+        .required(customValidationMessages.login.email.required)
+        .ensure()
+        .trim()
+        .lowercase(),
     }),
   },
 };
--- src/features/jobs/screens/CreateJob.tsx
+++ src/features/jobs/screens/CreateJob.tsx
@@ -1,69 +1,10 @@
 import React, { useState } from 'react';
-import { Animated, View } from 'react-native';
+import { View } from 'react-native';
 import { Box, Text, Button, ScrollBox } from '@ui/components';
 import Input from '@ui/components/Input';
 import { SafeAreaView } from 'react-native';
 
-const data = [
-  {
-    id: 1,
-    value: 'stone',
-    label: 'Stone',
-    bgColor: '#857f76',
-    textColor: 'white',
-  },
-  {
-    id: 2,
-    value: 'galvanised',
-    label: 'Galvanised',
-    bgColor: '#a7aaaf',
-    textColor: 'black',
-  },
-  {
-    id: 3,
-    value: 'zinc',
-    label: 'Zinc',
-    bgColor: '#b7b5b5',
-    textColor: 'black',
-  },
-  {
-    id: 4,
-    value: 'basalt',
-    label: 'Basalt',
-    bgColor: '#6e6c70',
-    textColor: 'white',
-  },
-  {
-    id: 5,
-    value: 'cream',
-    label: 'Cream',
-    bgColor: '#e8dec0',
-    textColor: '#8F94AE',
-  },
-  {
-    id: 6,
-    value: 'cottage_green',
-    label: 'Cottage Green',
-    bgColor: '#3b4c40',
-    textColor: 'white',
-  },
-  {
-    id: 7,
-    value: 'deep_ocean',
-    label: 'Deep Ocean',
-    bgColor: '#3b4252',
-    textColor: 'white',
-  },
-  {
-    id: 8,
-    value: 'manor_red',
-    label: 'Manor Red',
-    bgColor: '#532317',
-    textColor: 'white',
-  },
-];
-
-const CreateJobScreen = ({ navigation }) => {
+const CreateJobScreen = () => {
   const [name, setName] = useState('');
   const [jobNumber, setJobNumber] = useState('');
   const [siteAddress, setSiteAddress] = useState('');
