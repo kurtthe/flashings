7a3ee72a3f959feeda9fb98486531a7cb6436e19
Mateo Nieto <matheo.developer@gmail.com>
2023-08-24 17:59:45 -0500

    validation to next

--- src/core/services/general-request/alert.ts
+++ src/core/services/general-request/alert.ts
@@ -3,8 +3,8 @@ import { Alert } from 'react-native';
 class AlertService {
   show(title?: string, message?: string) {
     Alert.alert(
-      `${title || 'Alert'}`,
-      `${message || 'Error, please try again'}`,
+      `${title ?? 'Alert'}`,
+      `${message ?? 'Error, please try again'}`,
       [
         {
           text: 'Ok',
