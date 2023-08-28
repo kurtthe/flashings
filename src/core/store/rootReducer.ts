96bd83725bf5c7233b14a2030b7901228558550a
Mateo Nieto <matheo.developer@gmail.com>
2023-08-28 17:50:11 -0500

    add store flashing

--- src/core/store/rootReducer.ts
+++ src/core/store/rootReducer.ts
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import type { PayloadAction } from '@reduxjs/toolkit';
 
 import authReducer from './auth';
+import flashingReducer from './flashing'
 
 const combinedRootReducer = combineReducers({
   auth: authReducer,
+  flashings: flashingReducer,
 });
 
 function rootReducer(state: any, action: PayloadAction) {
