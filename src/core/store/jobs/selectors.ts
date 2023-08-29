aa0fa2f3694d2fe3bacb5251415036d3e75c2088
Jefferson Solano <solanojefferson@gmail.com>
2023-08-29 15:43:24 -0500

    change type jobs list

--- src/core/store/jobs/selectors.ts
+++ src/core/store/jobs/selectors.ts
@@ -1,9 +1,8 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from '@store';
 const jobsSelector = (state: RootState) => state.jobs;
-export const jobsListArchive = createSelector(jobsSelector, (state) => state.jobsArchive);
 
 export const jobsList = createSelector(jobsSelector,
 	(_:any, typeJob: 'current' | 'archived') => typeJob,
-	(state, typeJob) => typeJob === 'archived' ?state.jobsArchive: state.jobs);
+	(state, typeJob) => typeJob === 'current' ? state.jobs: state.jobsArchive);
 
