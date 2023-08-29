c766ed5c256bdad911ae49b3f67f9bd27f829104
Mateo Nieto <matheo.developer@gmail.com>
2023-08-29 18:14:55 -0500

    add flashing in the store

--- src/core/store/jobs/reducer.ts
+++ src/core/store/jobs/reducer.ts
@@ -6,21 +6,38 @@ import { persistConfigFlashings } from "@store/config";
 import { dataJobs, jobsArchive } from "@store/jobs/mocks";
 
 const INITIAL_STATE: JOB_STATE = {
-	jobs: dataJobs,
-	jobsArchive: jobsArchive
+    jobs: dataJobs,
+    jobsArchive: jobsArchive
 };
 
 const flashingReducer = createReducer(INITIAL_STATE, builder => {
-	builder.addCase(actions.addJob, (state, action) => {
-		const {job}= action.payload;
-		state.jobs = [...state.jobs, job]
-	});
-	builder.addCase(actions.loadJobs, (state, action)=> {
-		state.jobs = action.payload.jobs
-	});
-	builder.addCase(actions.loadJobsArchive, (state, action) =>{
-		state.jobsArchive = action.payload.jobs
-	})
+    builder.addCase(actions.addJob, (state, action) => {
+        const {job}= action.payload;
+        state.jobs = [...state.jobs, job]
+    });
+    builder.addCase(actions.loadJobs, (state, action)=> {
+        state.jobs = action.payload.jobs
+    });
+    builder.addCase(actions.loadJobsArchive, (state, action) =>{
+        state.jobsArchive = action.payload.jobs
+    });
+    builder.addCase(actions.addFlashing, (state, action)=>{
+
+    const jobId = action.payload.idJob
+    const dataFlashing =  action.payload.flashing
+
+    state.jobs = state.jobs.map((job)=> {
+        if(job.id === jobId){
+            return {
+                ...job,
+                flashings: [...job.flashings, dataFlashing]
+            }
+        }
+        return job
+    })
+
+
+    })
 })
 
 export type FlashingState = ReturnType<typeof flashingReducer>
