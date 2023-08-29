78ecbde8445a20bd1f42f227ac98ac2e0c911d44
Jefferson Solano <solanojefferson@gmail.com>
2023-08-29 18:14:55 -0500

    add flashing in the store

--- src/core/store/jobs/actions.ts
+++ src/core/store/jobs/actions.ts
@@ -1,8 +1,9 @@
 import {createAction} from "@reduxjs/toolkit";
-import { JOB_DATA } from "@models";
+import {FLASHINGS_DATA, JOB_DATA} from "@models";
 
 export const actions = {
 	addJob: createAction<{job:JOB_DATA}>('add/job'),
 	loadJobs: createAction<{jobs:JOB_DATA[]}>('load/jobs'),
 	loadJobsArchive: createAction<{jobs: JOB_DATA[]}>('load/jobs/archive'),
+	addFlashing: createAction<{flashing: FLASHINGS_DATA, idJob: number}>('jobs/add/flashing'),
 }
