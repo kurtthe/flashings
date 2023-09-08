caabdf6998f3a46807eb83caaa65a4b2f4b79bda
Mateo Nieto <matheo.developer@gmail.com>
2023-09-07 20:26:36 -0500

    textInput keyboard MeasurementLines

--- src/features/flashing/components/MeasurementLines.tsx
+++ src/features/flashing/components/MeasurementLines.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BaseTouchable, Box, Divider, Icon, IconButton, Text } from "@ui/components";
+import { BaseTouchable, Box, Divider, Icon,  Text } from "@ui/components";
 import { KeyBoardNumber } from '@features/flashing/components/KeyBoardNumber';
 import { LINE_SELECTED } from '@features/flashing/components/Board';
 import { isNaN } from "lodash";
 import { BackArrowIcon,  NextArrowIcon } from "@assets/icons";
+import { Keyboard, TextInput, TouchableWithoutFeedback } from "react-native";
 
 type Props = {
   onDone: (sizeLine: number) => void;
@@ -11,10 +12,12 @@ type Props = {
   onNext?: () => void;
   onPrevious?: () => void;
   disabledPrevious?: boolean
+  handleInput?: (visibleKeyBoard: boolean)=> void;
 };
-const MeasurementLines: React.FC<Props> = ({ onDone, dataLine, onNext, onPrevious , disabledPrevious=true}) => {
+const MeasurementLines: React.FC<Props> = ({ onDone, dataLine, onNext, onPrevious ,handleInput, disabledPrevious=true}) => {
   const [measurement, setMeasurement] = React.useState(0);
   const [currentValue, setCurrentValue] = React.useState('');
+  const inputRef = React.useRef(null)
 
   React.useEffect(() => {
     if (!dataLine) return;
@@ -39,31 +42,38 @@ const MeasurementLines: React.FC<Props> = ({ onDone, dataLine, onNext, onPreviou
   }
 
   return (
-    <Box p="s" >
-      <Box flexDirection="row" alignItems="center" justifyContent="space-around">
-        <Box disabled={disabledPrevious} as={BaseTouchable} onPress={handlePrevious}>
-          <Icon opacity={disabledPrevious? 0.3: 1} as={BackArrowIcon} size={22}  />
-        </Box>
-        <Text variant="subheadSecondary">Length:</Text>
-        <Box flexDirection="row" alignItems="center">
-          <Box backgroundColor="white" px="m" py="xxs">
-            <Text
-              variant="subheadMedium"
-            >{`${isNaN(measurement)? '0': measurement}`}</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <Box p="s" >
+        <Box flexDirection="row" alignItems="center" justifyContent="space-around">
+          <Box disabled={disabledPrevious} as={BaseTouchable} onPress={handlePrevious}>
+            <Icon opacity={disabledPrevious? 0.3: 1} as={BackArrowIcon} size={22}  />
           </Box>
-          <Text variant="bodyBold">mm</Text>
+          <Text variant="subheadSecondary">Length:</Text>
+          <Box flexDirection="row" alignItems="center">
+              <TextInput
+                ref={inputRef}
+                inputMode="numeric"
+                keyboardType="numeric"
+                onFocus={() => handleInput && handleInput(true)}
+                onBlur={() => handleInput  && handleInput(false)}
+                style={{textAlign: 'center', height: 30, width: 80, backgroundColor: 'white'}}
+                onChangeText={(newText: string)=> setCurrentValue(newText)}
+                value={`${isNaN(measurement)? '0': measurement}`}
+              />
+            <Text variant="bodyBold">mm</Text>
+          </Box>
+          <Box as={BaseTouchable} onPress={handleNext}>
+            <Icon as={NextArrowIcon} size={22}  />
+          </Box>
         </Box>
-        <Box as={BaseTouchable} onPress={handleNext}>
-          <Icon as={NextArrowIcon} size={22}  />
-        </Box>
+        <Divider my="s" />
+        <KeyBoardNumber
+          setCurrentValue={setCurrentValue}
+          currentValue={currentValue}
+          onChange={size => setMeasurement(parseFloat(size))}
+        />
       </Box>
-      <Divider my="s" />
-      <KeyBoardNumber
-        setCurrentValue={setCurrentValue}
-        currentValue={currentValue}
-        onChange={size => setMeasurement(parseFloat(size))}
-      />
-    </Box>
+    </TouchableWithoutFeedback>
   );
 };
 
