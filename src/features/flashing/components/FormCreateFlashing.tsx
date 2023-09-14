63d6e43887362e459280ca69d0cdc4feb8fe09dd
Mateo Nieto <matheo.developer@gmail.com>
2023-09-13 21:42:34 -0500

    before dynamics lengths

--- src/features/flashing/components/FormCreateFlashing.tsx
+++ src/features/flashing/components/FormCreateFlashing.tsx
@@ -4,17 +4,52 @@ import { Button, Box } from "@ui/components";
 import selectData from '../tempData/selectData.json'
 import { useFormikContext } from "formik";
 import { AddFlashingFormValues } from "@features/flashing/constants";
-import DismissKeyboardPressable from "@components/forms/DismissKeyboardPressable";
+import { FlatList } from "react-native";
 
+type SIZE_LENGTH_TYPE = {
+	qty: number;
+	length: number;
+}
 
-const FormCreateFlashingComponent = ()=>{
+const FormCreateFlashingComponent = ()=> {
+	const [sizesLength, setSizesLength] = React.useState<SIZE_LENGTH_TYPE[]>([{
+		qty: 0,
+		length: 0
+	}])
   const formik = useFormikContext<AddFlashingFormValues>();
   const { handleSubmit, isValid,isSubmitting } = formik;
 
+
+	const handleLength = () => {
+		setSizesLength((prevState)=> [...prevState, {qty: 0, length: 0} ])
+	}
+
+	const renderItem = ({item})=> (
+		<Box  flexDirection="row" justifyContent="space-between" mt="l" mb="unset" >
+			<FieldInput
+				isRequired
+				name="qty"
+				placeholder="qty"
+				label="Qty"
+				style={{width: 150}}
+				keyboardType="numeric"
+			/>
+			<FieldInput
+				isRequired
+				name="length"
+				placeholder="length"
+				label="Length"
+				style={{width: 150}}
+				suffix="mm"
+				keyboardType="numeric"
+			/>
+		</Box>
+	)
+
   return (
-			<DismissKeyboardPressable>
+
 	      <Box px="m" flex={1} >
-	        <Box my="m" flex={0.8}>
+	        <Box my="m" flex={0.9}>
 	          <FieldInput
 	            isRequired
 	            name="name"
@@ -31,29 +66,19 @@ const FormCreateFlashingComponent = ()=>{
 	            my="l"
 	          />
 
-	          <Box flexDirection="row" justifyContent="space-between" my="l" >
-	            <FieldInput
-	              isRequired
-	              name="qty"
-	              placeholder="qty"
-	              label="Qty"
-	              style={{width: 150}}
-	              keyboardType="numeric"
-	            />
-	            <FieldInput
-	              isRequired
-	              name="length"
-	              placeholder="length"
-	              label="Length"
-	              style={{width: 150}}
-	              suffix="mm"
-	              keyboardType="numeric"
-	            />
-	          </Box>
+		          <FlatList
+			          data={sizesLength}
+			          showsVerticalScrollIndicator={false}
+			          keyExtractor={(_,index)=> `row-length${index}`}
+			          renderItem={renderItem}
+			          style={{flexGrow: 1, height: '40%'}}
+		          />
 
 	            <Button
 	              variant="outlineWhite"
-	              mt="2xl">
+	              mt="2xl"
+	              onPress={handleLength}
+	              >
 	              + Add Length
 	            </Button>
 	          </Box>
@@ -64,7 +89,8 @@ const FormCreateFlashingComponent = ()=>{
 	            Start Drawing
 	          </Button>
 	      </Box>
-    </DismissKeyboardPressable>
+
+
   )
 }
 
