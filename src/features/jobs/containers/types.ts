2ee41b32f00121277723046fb094e01a895bab9d
Mateo Nieto <matheo.developer@gmail.com>
2023-09-26 21:34:28 -0500

    load data job form

--- src/features/jobs/containers/types.ts
+++ src/features/jobs/containers/types.ts
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
 import { forms } from '../constants';
 
-export type CreateFormValues = Yup.InferType<typeof forms.createJob.schema> & {
+export type CreateEditFormValues = Yup.InferType<typeof forms.createEditJob.schema> & {
   submit?: string;
 };
