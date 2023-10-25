3c16557523322328572d8a39562d96c98a38ea3b
Jefferson Solano <solanojefferson@gmail.com>
2023-10-24 19:18:00 -0500

    profile screen

---
+++ src/features/profile/screens/Profile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box, Text } from "@ui/components";
+
+const ProfileScreen = () => {
+	 return (
+		 <Box>
+			 <Text>profile information</Text>
+		 </Box>
+	 )
+}
+export default ProfileScreen
