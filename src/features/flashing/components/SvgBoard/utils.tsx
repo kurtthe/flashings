3ed11f1cff38487d38f86efc92b51d0325a83769
Mateo Nieto <matheo.developer@gmail.com>
2023-11-03 18:53:08 -0500

    vertical and horizontals

--- src/features/flashing/components/SvgBoard/utils.tsx
+++ src/features/flashing/components/SvgBoard/utils.tsx
@@ -7,6 +7,7 @@ const calculateTypeLine = ({start=true, type, line, isRight=true}: BREAK_END_STA
 	const sizeLine = 20;
 	let anglesRight = start? 40: 60;
 	let anglesLeft = start? 5: 20;
+
 	if(type.includes('1')){
 		anglesRight = start? 80: 40;
 		anglesLeft = start? 20: 30;
@@ -26,8 +27,10 @@ const calculateTypeLine = ({start=true, type, line, isRight=true}: BREAK_END_STA
 }
 
 const calculatePointsParabola = (dataLine:LINE_TYPE, parallelRight= true )=> {
-	const marginVertical = 5
-	const {points, pending} = dataLine
+	const {points} = dataLine
+	let radiusEllipseX = 5
+	let radiusEllipseY = 10
+
 	const pointX1 = points[0][0];
 	const pointX2 = points[1][0];
 
@@ -38,32 +41,100 @@ const calculatePointsParabola = (dataLine:LINE_TYPE, parallelRight= true )=> {
 	const isVertical = pointX1 === pointX2;
 
 	if(isHorizontal){
-		return `M${pointX1} ${pointY1} Q ${pointX1} ${pointY1 * 1.2} ${pointX1 * 1.3} ${pointY1}`
+		if(pointX2 > pointX1){
+			if(parallelRight){
+				return {points: [[pointX1 + 3, pointY1 + 10]], radius: {
+					x: radiusEllipseX,
+					y: radiusEllipseY
+					}}
+			}
+			return {points:  [[pointX1 + 3, pointY1 - 9]], radius: {
+					x: radiusEllipseX,
+					y: radiusEllipseY
+				}}
+		}
+		if(parallelRight){
+			return { points: [[pointX1, pointY1 - 10]], radius: {
+					x: radiusEllipseX,
+					y: radiusEllipseY
+				} }
+		}
+		return {
+			points: [[pointX1, pointY1 + 10]],
+			radius: {
+				x: radiusEllipseX,
+				y: radiusEllipseY
+			}
+		}
 	}
-
+// vertical
 	if(isVertical){
-		return `M${pointX1} ${pointY1} Q ${(pointX1- 20) * -1 } ${pointY1} ${pointX1} ${pointY1 * 1.2}`
+		if(pointY1 > pointY2){
+			if(parallelRight){
+				return {
+					points: [[pointX1 + 13, pointY1 + 10]],
+					radius: {
+						x: radiusEllipseX,
+						y: radiusEllipseY
+					}
+				}
+			}
+			return {
+				points: [[pointX1 + 23, pointY1 + 10]],
+				radius: {
+					x: radiusEllipseX,
+					y: radiusEllipseY
+				}
+			}
+		}
+		if(parallelRight){
+			return {
+				points: [[pointX1 - 10, pointY1]],
+				radius: {
+					x: radiusEllipseY,
+					y: radiusEllipseX
+				}
+			}
+		}
+		return {
+			points: [[pointX1 + 10, pointY1]],
+			radius: {
+				x: radiusEllipseY,
+				y: radiusEllipseX
+			}
+		}
 	}
 
-	return `M${pointX1} ${pointY1} Q ${pointX1} ${pointY1 * 1.2} ${pointX1 * 1.3} ${pointY1}`
+	return {
+		points: [[pointX1, pointY1]],
+		radius: {
+			x: radiusEllipseX,
+			y: radiusEllipseY
+		}
+	}
 }
 
 export const getEndStartTypeLine = ({typeStart, typeEnd, isRightBlueLine, lineEnd, lineStart}:START_END_LINE_TYPE )=>{
-	const pointsStart = calculateTypeLine({ type: typeStart, line: lineStart, start:true, isRight: isRightBlueLine })
-	const pointsEnd = calculateTypeLine({ type: typeEnd, line: lineEnd, start:false, isRight: isRightBlueLine })
+	const pointsStartPath = calculateTypeLine({ type: typeStart, line: lineStart, start:true, isRight: isRightBlueLine })
+	const pointsEndPath = calculateTypeLine({ type: typeEnd, line: lineEnd, start:false, isRight: isRightBlueLine })
 
 	const isSafetyStart = typeStart.includes('safety')
 	const isSafetyEnd = typeEnd.includes('safety')
 
-	const radiusEllipseX = 5
-	const radiusEllipseY = 15
+	const isStartLine = typeStart.includes('Start')
+	console.log("typeStart::", typeStart)
+	console.log("isStartLine::", isStartLine)
+	const isEndLine = typeEnd.includes('end')
+
+	const {points: pointsStart, radius: radiusStart} = calculatePointsParabola(lineStart, isStartLine)
+	const {points: pointsEnd, radius: radiusEnd} = calculatePointsParabola(lineEnd, isEndLine)
 
 	return (
 		<>
 			{
 				!isSafetyStart && (
 					<PathComponent
-						d={buildPathLine(pointsStart)}
+						d={buildPathLine(pointsStartPath)}
 						strokeWidth={1}
 						stroke="#000"
 						fill="none"
@@ -72,13 +143,13 @@ export const getEndStartTypeLine = ({typeStart, typeEnd, isRightBlueLine, lineEn
 			}
 			{
 				isSafetyStart && (
-					<Ellipse cx={pointsStart[0][0]} cy={pointsStart[0][1]} rx={radiusEllipseX} ry={radiusEllipseY} stroke="black" fill="black" />
+					<Ellipse cx={pointsStart[0][0]} cy={pointsStart[0][1]} rx={radiusStart.x} ry={radiusStart.y} stroke="black" fill="transparent" />
 				)
 			}
 			{
 				!isSafetyEnd && (
 					<PathComponent
-						d={buildPathLine(pointsEnd)}
+						d={buildPathLine(pointsEndPath)}
 						strokeWidth={1}
 						stroke="#000"
 						fill="none"
@@ -87,7 +158,7 @@ export const getEndStartTypeLine = ({typeStart, typeEnd, isRightBlueLine, lineEn
 			}
 			{
 				isSafetyEnd && (
-					<Ellipse cx={pointsEnd[0][0]} cy={pointsEnd[0][1]} rx={radiusEllipseX} ry={radiusEllipseY} stroke="black" fill="black" />
+					<Ellipse cx={pointsEnd[0][0]} cy={pointsEnd[0][1]} rx={radiusEnd.x} ry={radiusEnd.y} stroke="black" fill="black" />
 				)
 			}
 		</>
