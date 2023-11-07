83d69f29bc3e239d8f89424e03c549e8c80ba4a6
Mateo Nieto <matheo.developer@gmail.com>
2023-11-06 20:20:14 -0500

    view shot flashing

--- src/features/flashing/components/PreviewFlashing/PreviewFlashing.tsx
+++ src/features/flashing/components/PreviewFlashing/PreviewFlashing.tsx
@@ -4,67 +4,33 @@ import { FLASHINGS_DATA } from "@models";
 import { buildLinesForPreview } from "@features/flashing/components/PreviewFlashing/utils";
 import { Path, serialize } from "react-native-redash";
 import { Box } from "@ui/components";
-import {TouchableOpacity}  from "react-native";
+import { Dimensions, Image, TouchableOpacity } from "react-native";
 import { ModalBottom, ModalBottomRef } from '@components';
 import { BoardComponent } from "@features/flashing/components";
 import { calculateAngle, getIndexOfStepForName } from "@features/flashing/utils";
 
 type Props = {
-	width: number;
-	height: number;
-	dataFlashing: FLASHINGS_DATA;
+	width?: number;
+	height?: number;
+	imgPreview: string | undefined;
 }
-const PreviewFlashing: React.FC<Props> = ({width,height, dataFlashing}) => {
-	const [pathLines, setPathLines ] = React.useState<Path | null>(null)
+const PreviewFlashing: React.FC<Props> = ({ imgPreview}) => {
 	const modalBottomRef = React.useRef<ModalBottomRef>();
-	const [anglesLines, setAnglesLines] = React.useState<number[]>([]);
-
-	React.useEffect(() => {
-		const makingLines = buildLinesForPreview(dataFlashing.dataLines, width, height);
-		setPathLines(makingLines)
-	}, [dataFlashing.dataLines]);
-
-	React.useEffect(()=>{
-
-		if(dataFlashing.dataLines.length < 2) return
-
-		const newAngles = dataFlashing.dataLines.map((line, index, arrayLines)=> {
-			if(!anglesLines[index]){
-				return calculateAngle(line, arrayLines[index + 1])?? 0
-			}
-			return anglesLines[index]
-		})
-		setAnglesLines(newAngles)
-	}, [dataFlashing.dataLines])
 
 	return (
 		<>
 			<TouchableOpacity onPress={() => modalBottomRef.current?.show()}>
-			<Box backgroundColor="white" width={width} height={height} px="s" mt="xs" borderWidth={1.5} borderColor="divider" borderRadius="s" alignItems="center" justifyContent="center"  >
-				<Svg width={width/2} height={height - 10}>
-					{pathLines && <PathComponent
-						d={serialize(pathLines)}
-						stroke={"#000"}
-						strokeWidth={1}
-						fill="transparent"
-					/>}
-				</Svg>
-			</Box>
+				{imgPreview && <Image source={{ uri: imgPreview }} width={120} height={92} />}
 			</TouchableOpacity>
 
 			<ModalBottom
 				backdropClosesSheet={true}
 				ref={modalBottomRef}
-				height={500}
+				height={600}
 				draggable={false}
 			>
-				<Box style={{marginTop: -100}} backgroundColor="white">
-					<BoardComponent
-						rightLinePaint={dataFlashing.parallelRight}
-						lines={dataFlashing.dataLines}
-						angles={anglesLines}
-						stepBoard={getIndexOfStepForName('preview')}
-					/>
+				<Box backgroundColor="white">
+					{<Image source={{ uri: imgPreview }} width={Dimensions.get("screen").width} height={600} />}
 				</Box>
 			</ModalBottom>
 		</>
