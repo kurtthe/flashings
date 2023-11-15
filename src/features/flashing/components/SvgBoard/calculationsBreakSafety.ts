4914d147f2f98276049a7e232af73dc9401a52b0
Jefferson Solano <solanojefferson@gmail.com>
2023-11-14 21:47:21 -0500

    rotation ellipse

--- src/features/flashing/components/SvgBoard/calculationsBreakSafety.ts
+++ src/features/flashing/components/SvgBoard/calculationsBreakSafety.ts
@@ -2,6 +2,13 @@ import { BREAK_END_START_LINE_TYPE, LINE_TYPE, POINT_TYPE, START_END_LINE_TYPE, 
 import { isNaN } from "lodash";
 import { createEquationOfLine } from "@features/flashing/utils";
 
+const equationResultAzimuth = {
+	'1': 'angle',
+	'2': '180-angle',
+	'3': '180+angle',
+	'4': '360-angle'
+}
+
 const anglesEachType = {
 	default: 45,
 	horizontal: {
@@ -92,7 +99,8 @@ export const calculateTypeLine = ({points, angle}: BREAK_END_START_LINE_TYPE): P
 }
 
 export const calculatePointsParabola = (dataLine:LINE_TYPE,  typeLine : TYPE_END_LINES, endPoints= false )=> {
-	const {points, pending} = dataLine
+	const {points} = dataLine
+	const pending = calculateAngle(dataLine)
 	let radiusEllipseX = 4
 	let radiusEllipseY = 10
 
@@ -336,5 +344,26 @@ export const getAngleForTheLine = (line:LINE_TYPE, typeLine: TYPE_END_LINES) => 
 	return anglesEachType.default
 }
 
+const calculateAngle = (lineData: LINE_TYPE)=>{
+	const coordinatesM = lineData.points[0];
+	const coordinatesN = lineData.points[1];
+
+	const deltaN = coordinatesN[0] - coordinatesM[0];
+	const deltaE = coordinatesN[1] - coordinatesM[1];
+
+	const quadrant1 = deltaE > 0 && deltaN > 0 && '1'
+	const quadrant2 = deltaE > 0 && deltaN < 0 && '2'
+	const quadrant3 = deltaE < 0 && deltaN < 0 && '3'
+	const quadrant4 = deltaE < 0 && deltaN > 0 && '4'
+
+	const quadrant = quadrant1 ?? quadrant2 ?? quadrant3 ?? quadrant4
+
+	const thetaAngle = Math.atan(deltaE/deltaN)
+	const angleRadians = thetaAngle * (180 / Math.PI)
+
+	const equationToResolve = equationResultAzimuth[!quadrant? '1': quadrant]
+	const replaceValues = equationToResolve.replace('angle', `${angleRadians}`)
+	return eval(replaceValues)
+}
 
 
