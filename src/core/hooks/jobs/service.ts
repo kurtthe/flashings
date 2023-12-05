52d4bdc6f5bb83cddd69ca6d94a342250368e7a9
Mateo Nieto <matheo.developer@gmail.com>
2023-12-04 21:45:41 -0500

    headers request services

--- src/core/hooks/jobs/service.ts
+++ src/core/hooks/jobs/service.ts
@@ -1,5 +1,16 @@
-import { JOB_DATA } from "@models";
+import { JOB_DATA, STORE } from "@models";
+import { endPoints } from "@shared/endPoints";
+import { RequestService } from '@services/index';
 
 export const jobService = (dataJob:JOB_DATA ): Promise<JOB_DATA> => {
 	return Promise.resolve(dataJob);
+}
+
+export const getStores = async (): Promise<STORE[]>=> {
+	const response = await RequestService.get<STORE[]>(endPoints.getStores)
+	return Promise.resolve(response.body)
+}
+
+export const queryKey = {
+	get_stores: 'get_stores'
 }
--- src/features/jobs/screens/OrderSummary.tsx
+++ src/features/jobs/screens/OrderSummary.tsx
@@ -1,14 +1,67 @@
 import React from 'react';
 import { Box, Button } from "@ui/components";
+import Pdf from 'react-native-pdf';
+import { StyleSheet } from "react-native";
+import { useGetStores } from "@hooks/jobs";
 
+type Props = {
+	urlPdf: string;
+}
+const OrderSummaryScreen: React.FC<Props> = ({urlPdf}) => {
+	const {data: stores } = useGetStores();
 
-const OrderSummaryScreen = () => {
+	const source = {
+		uri: 'https://s29.q4cdn.com/175625835/files/doc_downloads/test.pdf',
+		cache: true,
+	};
 
 	return (
-	<Box p="m">
-		<Button variant="solidWhite">Shared</Button>
+	<Box p="m" style={styles.container}>
+		<Pdf
+			source={source}
+			style={styles.pdf}
+			onLoadComplete={(numberOfPages,filePath) => {
+				console.log(`Number of pages: ${numberOfPages}`);
+			}}
+			onPageChanged={(page,numberOfPages) => {
+				console.log(`Current page: ${page}`);
+			}}
+			onError={(error) => {
+				console.log(error);
+			}}
+			onPressLink={(uri) => {
+				console.log(`Link pressed: ${uri}`);
+			}}
+		/>
+		<Button
+			variant="solidWhite"
+			borderRadius="unset"
+			borderWidth={1}
+			color="black"
+			my="s"
+		>Print</Button>
+		<Button
+			variant="solidWhite"
+			borderRadius="unset"
+			borderWidth={1}
+			color="black"
+		>Shared</Button>
+
+
+		<Button
+			my="m"
+			variant="solid"
+		>Send to store</Button>
 	</Box>
 	)
 }
-
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'flex-start',
+	},
+	pdf: {
+		flex:0.8,
+	}
+});
 export default OrderSummaryScreen
