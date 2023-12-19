535753e0a9ac5de0ab1eccc6ce317a68f4399ae1
Mateo Nieto <matheo.developer@gmail.com>
2023-12-19 18:48:49 -0500

    gif

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import Lottie from "@components/Lottie";
-import { Box } from "@ui/components";
-import { lotties } from "@assets/lotties";
-import { Text } from "react-native";
+import { Box, Text } from "@ui/components";
+import { Image } from "react-native";
 
 type Props = {
 	title?: string;
 }
 const Loading: React.FC<Props> = ({title}) => {
 	return (
-		<Box flex={1}>
-			<Lottie source={lotties.loading} autoPlay loop />
-			{title && <Text>{title}</Text>}
+		<Box flex={1} alignItems="center" justifyContent="center" backgroundColor="white">
+			<Image source={require("@assets/images/loading-wheel.gif")} width={150} height={100}  />
+			{title && <Text variant="subheadBold">{title}</Text>}
 		</Box>
 	)
 }
