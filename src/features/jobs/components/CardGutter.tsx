4497a404794c37c33354b893a54766116f7ab68e
Mateo Nieto <matheo.developer@gmail.com>
2024-04-24 20:09:49 -0500

    warning preview

--- src/features/jobs/components/CardGutter.tsx
+++ src/features/jobs/components/CardGutter.tsx
@@ -1,86 +1,123 @@
 import React from 'react';
-import { Box, Button, Card, CardProps, Text } from "@ui/components";
-import { FLASHINGS_DATA } from "@models";
-import PreviewFlashing from "@features/flashing/components/PreviewFlashing";
-import ModalAddLengths from "@features/jobs/components/ModalAddLengths";
-import { FlatList } from "react-native";
-import { StackPrivateDefinitions, StackPrivateProps } from "@routes/PrivateNavigator";
-import { useNavigation } from "@react-navigation/native";
-import { Routes } from "@features/flashing/navigation/routes";
-import { getBends, getGirth, getMaterial } from "@features/jobs/utils";
+import { Box, Button, Card, CardProps, Text } from '@ui/components';
+import { FLASHINGS_DATA } from '@models';
+import ModalAddLengths from '@features/jobs/components/ModalAddLengths';
+import { FlatList } from 'react-native';
+import {
+  StackPrivateDefinitions,
+  StackPrivateProps,
+} from '@routes/PrivateNavigator';
+import { useNavigation } from '@react-navigation/native';
+import { Routes } from '@features/flashing/navigation/routes';
+import { getBends, getGirth, getMaterial } from '@features/jobs/utils';
+import PreviewFlashing from '@features/flashing/components/PreviewFlashing/PreviewFlashing';
 
 type Props = CardProps & {
   data: FLASHINGS_DATA;
   onAddLength?: () => void;
   jobId: number;
-}
-const CardGutterComponent: React.FC<Props> = ({data, onAddLength, jobId, ...rest})=>{
-  const [visibleModalLength, setVisibleModalLength] = React.useState(false)
-  const navigation = useNavigation<StackPrivateProps>()
+};
+const CardGutterComponent: React.FC<Props> = ({
+  data,
+  onAddLength,
+  jobId,
+  ...rest
+}) => {
+  const [visibleModalLength, setVisibleModalLength] = React.useState(false);
+  const navigation = useNavigation<StackPrivateProps>();
 
-
-  const handleEditFlashing = ()=> {
+  const handleEditFlashing = () => {
     navigation.navigate(StackPrivateDefinitions.FLASHING, {
       screen: Routes.CREATE_EDIT_FLASHING,
-      params: {jobId: jobId, idFlashing: data.id}
+      params: { jobId: jobId, idFlashing: data.id },
     });
-  }
+  };
 
   const renderFlashingLengths = () => {
-    if(!data.flashingLengths) return null
+    if (!data.flashingLengths) return null;
 
     return (
       <Box flexDirection="row">
         <FlatList
           scrollEnabled={false}
-          keyExtractor={(item, index)=>`textLength-${index}${item.qty}`}
+          keyExtractor={(item, index) => `textLength-${index}${item.qty}`}
           data={data.flashingLengths}
-          renderItem={({item, index})=>
+          renderItem={({ item, index }) => (
             <Box flexDirection="row" flexWrap="wrap">
               <Text variant="bodySmallRegular">
                 {item.length}mm x {item.qty}
               </Text>
-              {(data.flashingLengths.length ===  (index +1)) && <Button isDisabled={data.flashingLengths.length > 7} variant="textSmall" onPress={() => data.flashingLengths.length > 7? null: setVisibleModalLength(true)}>
-                +ADD LENGTH
-              </Button>
-              }
+              {data.flashingLengths.length === index + 1 && (
+                <Button
+                  isDisabled={data.flashingLengths.length > 7}
+                  variant="textSmall"
+                  onPress={() =>
+                    data.flashingLengths.length > 7
+                      ? null
+                      : setVisibleModalLength(true)
+                  }>
+                  +ADD LENGTH
+                </Button>
+              )}
             </Box>
-          }
+          )}
         />
       </Box>
-    )
-  }
+    );
+  };
 
   return (
     <>
-      <Card pb="unset" flexDirection="row" alignItems="flex-start" justifyContent="space-between" {...rest}>
-        <Box  width='50%'>
-          <Text mb="m" variant="bodyBold" >{data.name !== '' ? data.name: 'Flashing'}</Text>
-          <PreviewFlashing imgPreview={data.imgPreview} dataFlashing={data}/>
+      <Card
+        pb="unset"
+        flexDirection="row"
+        alignItems="flex-start"
+        justifyContent="space-between"
+        {...rest}>
+        <Box width="50%">
+          <Text mb="m" variant="bodyBold">
+            {data.name !== '' ? data.name : 'Flashing'}
+          </Text>
+          <PreviewFlashing imgPreview={data.imgPreview} dataFlashing={data} />
         </Box>
-        <Box width='45%'>
-          <Box
-            flexDirection='row'
-            justifyContent='flex-start'
-            mb="xs"
-          >
-            <Text lineHeight={18} onPress={handleEditFlashing} variant="linkTextSmall">Edit</Text>
+        <Box width="45%">
+          <Box flexDirection="row" justifyContent="flex-start" mb="xs">
+            <Text
+              lineHeight={18}
+              onPress={handleEditFlashing}
+              variant="linkTextSmall">
+              Edit
+            </Text>
           </Box>
           <Box>
-            <Text variant="bodyLabelTextfield" fontWeight="bold" color="black" >Description</Text>
-            <Text variant="bodySmallRegular" style={{textTransform: 'capitalize'}}>
+            <Text variant="bodyLabelTextfield" fontWeight="bold" color="black">
+              Description
+            </Text>
+            <Text
+              variant="bodySmallRegular"
+              style={{ textTransform: 'capitalize' }}>
               0.55 Colorbond {getMaterial(data.colourMaterial).label}
             </Text>
-            <Box  flexDirection="row" alignItems="flex-start" justifyContent="flex-start">
+            <Box
+              flexDirection="row"
+              alignItems="flex-start"
+              justifyContent="flex-start">
               {renderFlashingLengths()}
             </Box>
-            <Text variant="bodySmallRegular">{getBends(data)} Bend Girth - {`${getGirth(data)}mm`}</Text>
+            <Text variant="bodySmallRegular">
+              {getBends(data)} Bend Girth - {`${getGirth(data)}mm`}
+            </Text>
           </Box>
         </Box>
       </Card>
-      <ModalAddLengths idFlashing={data.id} jobId={jobId} visible={visibleModalLength} onClose={()=> setVisibleModalLength(false)} />
+      <ModalAddLengths
+        idFlashing={data.id}
+        jobId={jobId}
+        visible={visibleModalLength}
+        onClose={() => setVisibleModalLength(false)}
+      />
     </>
-  )
-}
+  );
+};
 
-export default  CardGutterComponent
+export default CardGutterComponent;
