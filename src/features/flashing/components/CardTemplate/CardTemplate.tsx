b66b1e20edf453497556bf657d1a485c8cf389a7
Mateo Nieto <matheo.developer@gmail.com>
2024-07-03 16:55:27 -0500

    footer cards templates

--- src/features/flashing/components/CardTemplate/CardTemplate.tsx
+++ src/features/flashing/components/CardTemplate/CardTemplate.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TemplateType } from '@models/templates';
-import { BaseTouchable, Card, Text } from '@ui/components';
+import { BaseTouchable, Box, Button, Card, Text } from '@ui/components';
 import { Image } from 'react-native';
 import { useAppDispatch } from '@hooks/useStore';
 import { actions as templateActions } from '@store/templates/actions';
@@ -20,24 +20,38 @@ const CardTemplate: React.FC<Props> = ({ template }) => {
 
   return (
     <Card borderWidth={1} width="46%" mx="s">
-      <BaseTouchable onPress={onCardTemplate}>
-        <Image
-          resizeMode="contain"
-          source={{
-            uri: template.imgPreview,
-          }}
-          width={110}
-          height={180}
-          style={{
-            transform: [{ scale: 1.8 }],
-            top: 15,
-            left: 15,
-          }}
-        />
-        <Text mx="s" mt="s" variant="bodyBold">
-          {template.name}
-        </Text>
-      </BaseTouchable>
+      <>
+        <BaseTouchable onPress={onCardTemplate}>
+          <Image
+            resizeMode="contain"
+            source={{
+              uri: template.imgPreview,
+            }}
+            width={110}
+            height={180}
+            style={{
+              transform: [{ scale: 1.8 }],
+              top: 15,
+              left: 15,
+            }}
+          />
+          <Text mx="s" mt="s" variant="bodyBold">
+            {template.name}
+          </Text>
+        </BaseTouchable>
+        <Box
+          flexDirection="row"
+          alignItems="center"
+          justifyContent="space-between"
+          py="m">
+          <Button variant="delete" onPress={() => null}>
+            Delete
+          </Button>
+          <Button variant="small" onPress={() => null}>
+            Rename
+          </Button>
+        </Box>
+      </>
     </Card>
   );
 };
