326915af9ce643bc38f264fdb43c8a8304afdbd7
Mateo Nieto <matheo.developer@gmail.com>
2024-07-04 23:01:06 -0500

    all templates

--- src/core/store/templates/selectors.ts
+++ src/core/store/templates/selectors.ts
@@ -3,10 +3,15 @@ import { RootState } from '@store';
 
 const templatesSelector = (state: RootState) => state.templates;
 
-export const templatesList = createSelector(
+export const allTemplatesList = createSelector(
   templatesSelector,
   state => state.templates,
 );
+
+export const templatesList = createSelector(templatesSelector, state =>
+  state.templates.filter(item => !item.isHide),
+);
+
 export const templateSelected = createSelector(templatesSelector, state => {
   return state.templates.find(item => item.id === state.templateSelected);
 });
