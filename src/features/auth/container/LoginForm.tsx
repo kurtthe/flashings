a56033de8a788807c7fc2f5c58ba1348d4552f18
Mateo Nieto <matheo.developer@gmail.com>
2024-07-10 15:20:59 -0500

    login form

--- src/features/auth/container/LoginForm.tsx
+++ src/features/auth/container/LoginForm.tsx
@@ -7,18 +7,18 @@ import { LOGIN_RESPONSE } from '@models';
 import { LoginFormComponent } from '@features/auth/components';
 import { useAppDispatch } from '@hooks/useStore';
 import { actions as authActions } from '@store/auth/actions';
-import { Box, Text } from "@ui/components";
-import SimpleButton from "@components/SimpleButton";
-import { Routes } from "@features/auth/navigation/routes";
-import { useNavigation } from "@react-navigation/native";
-import { AuthStackProps } from "@features/auth/navigation/Stack.types";
+import { Box, Text } from '@ui/components';
+import SimpleButton from '@components/SimpleButton';
+import { Routes } from '@features/auth/navigation/routes';
+import { useNavigation } from '@react-navigation/native';
+import { AuthStackProps } from '@features/auth/navigation/Stack.types';
 const LoginForm = () => {
-  const navigation = useNavigation<AuthStackProps>()
+  const navigation = useNavigation<AuthStackProps>();
   const dispatch = useAppDispatch();
 
   const formikRef = React.useRef<FormikProps<LoginFormValues>>(null);
 
-  const { mutate: doLogin } = useLogin({
+  const { mutate: doLogin, isLoading } = useLogin({
     onSuccess: data => {
       dispatch(authActions.signIn({ data: data as LOGIN_RESPONSE }));
     },
@@ -51,7 +51,7 @@ const LoginForm = () => {
         initialErrors={forms.login.initialErrors}
         validationSchema={forms.login.schema}
         onSubmit={handleSubmit}>
-        <LoginFormComponent />
+        <LoginFormComponent isLoading={isLoading} />
       </Formik>
 
       <Box alignItems="center" flex={1} py="s" mb="s" backgroundColor="white">
@@ -61,7 +61,7 @@ const LoginForm = () => {
           onPress={() => navigation.navigate(Routes.HELP_SUPPORT)}>
           Need Help?
         </SimpleButton>
-        <Box flexDirection='column' alignItems="center" py="s">
+        <Box flexDirection="column" alignItems="center" py="s">
           <Text style={{ color: '#444857', fontSize: 15 }}>
             Don't have an account yet?
           </Text>
