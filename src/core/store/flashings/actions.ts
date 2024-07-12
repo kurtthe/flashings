3e0c6a133f02ab137f6bab42fe054591492cd8a2
Mateo Nieto <matheo.developer@gmail.com>
2024-07-11 20:30:03 -0500

    flashing reducers

--- src/core/store/flashings/actions.ts
+++ src/core/store/flashings/actions.ts
@@ -1,8 +1,16 @@
 import { createAction } from '@reduxjs/toolkit';
 import { SIDE_TAPERED_TYPES } from '@store/flashings/reducer';
+import { FLASHINGS_DATA } from '@models';
 
 export const actions = {
   changeSideTapered: createAction<{ side: SIDE_TAPERED_TYPES }>(
     'changeSideTapered/flashing',
   ),
+  changeStep: createAction<{ step: number }>('changeStep/flashing'),
+  addFlashingDraft: createAction<{ dataFlashing: FLASHINGS_DATA }>(
+    'addDraft/flashing',
+  ),
+  updateFlashingDraft: createAction<{
+    dataFlashing: FLASHINGS_DATA;
+  }>('updateDraft/flashing'),
 };
--- src/core/store/flashings/reducer.ts
+++ src/core/store/flashings/reducer.ts
@@ -1,16 +1,39 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { actions } from '@store/flashings/actions';
+import { FLASHINGS_DATA } from '@models';
 
 export type SIDE_TAPERED_TYPES = 'front' | 'back';
 
-const INITIAL_STATE = {
+type initialStateType = {
+  sideTapered: SIDE_TAPERED_TYPES;
+  stepIndex: number;
+  flashingDraft: FLASHINGS_DATA | undefined;
+};
+
+const INITIAL_STATE: initialStateType = {
   sideTapered: 'front',
+  stepIndex: 0,
+  flashingDraft: undefined,
 };
 
 const flashingsReducer = createReducer(INITIAL_STATE, builder => {
   builder.addCase(actions.changeSideTapered, (state, action) => {
     const { side } = action.payload;
     state.sideTapered = side;
+  });
+  builder.addCase(actions.changeStep, (state, action) => {
+    const { step } = action.payload;
+    state.stepIndex = step;
+  });
+
+  builder.addCase(actions.addFlashingDraft, (state, action) => {
+    const { dataFlashing } = action.payload;
+    state.flashingDraft = dataFlashing;
+  });
+
+  builder.addCase(actions.updateFlashingDraft, (state, action) => {
+    const { dataFlashing } = action.payload;
+    state.flashingDraft = { ...state.flashingDraft, ...dataFlashing };
   });
 });
 
