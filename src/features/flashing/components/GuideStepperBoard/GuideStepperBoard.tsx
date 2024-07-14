0885d76cc0fcd9558b3ea349c47da6ab3f4b6afc
Mateo Nieto <matheo.developer@gmail.com>
2024-07-13 20:35:44 -0500

    guider step

--- src/features/flashing/components/GuideStepperBoard/GuideStepperBoard.tsx
+++ src/features/flashing/components/GuideStepperBoard/GuideStepperBoard.tsx
@@ -1,93 +1,127 @@
 import React from 'react';
-import { Box, Button, Card, Divider, Text } from "@ui/components";
-import { Dimensions, StyleSheet } from "react-native";
+import { Box, Button, Card, Divider, Text } from '@ui/components';
+import { Dimensions, StyleSheet } from 'react-native';
 import {
-	GUIDE_STEP,
-	guideSteps, TYPE_ACTIONS_STEP,
-	VALUE_ACTIONS
-} from "@features/flashing/components/GuideStepperBoard/GuideStepperBoard.type";
+  GUIDE_STEP,
+  guideSteps,
+  TYPE_ACTIONS_STEP,
+  VALUE_ACTIONS,
+} from '@features/flashing/components/GuideStepperBoard/GuideStepperBoard.type';
+import { useAppSelector } from '@hooks/useStore';
+import { getStep } from '@store/flashings/selectors';
+import { getIndexOfStepForName } from '@features/flashing/utils';
 
 type Props = {
-	step: number;
-	onFinish: ()=>void;
-	onChangeOption?: (newValue: VALUE_ACTIONS)=> void;
-}
+  step: number;
+  onFinish: () => void;
+  onChangeOption?: (newValue: VALUE_ACTIONS) => void;
+};
 
-const heightScreen = Dimensions.get("screen").height
-const GuideStepperBoardComponent: React.FC<Props> = ({ onFinish, step,onChangeOption }) => {
-	const [{ action, description, title }, setDataStep] = React.useState<GUIDE_STEP>(guideSteps[step])
-	const [optionSelected, setOptionSelected] = React.useState<VALUE_ACTIONS>({
-		[TYPE_ACTIONS_STEP.SIDE_PAINT_EDGE]: action?.defaultOption ?? 'right'
-	})
+const heightScreen = Dimensions.get('screen').height;
+const GuideStepperBoardComponent: React.FC<Props> = ({
+  onFinish,
+  step,
+  onChangeOption,
+}) => {
+  const stepBoard = useAppSelector(state => getStep(state));
 
-	React.useEffect(() =>{
-		const lengthSteps = guideSteps.length
-		const newDataStep = guideSteps[step >= lengthSteps? lengthSteps -1: step ];
+  const [{ action, description, title }, setDataStep] =
+    React.useState<GUIDE_STEP>(guideSteps[step]);
+  const [optionSelected, setOptionSelected] = React.useState<VALUE_ACTIONS>({
+    [TYPE_ACTIONS_STEP.SIDE_PAINT_EDGE]: action?.defaultOption ?? 'right',
+    [TYPE_ACTIONS_STEP.SIDE_TAPERED]: action?.defaultOption ?? 'front',
+  });
 
-		setDataStep(newDataStep)
-		step >= lengthSteps && onFinish()
+  React.useEffect(() => {
+    const lengthSteps = guideSteps.length;
+    const newDataStep =
+      guideSteps[step >= lengthSteps ? lengthSteps - 1 : step];
 
-	}, [step, guideSteps])
+    setDataStep(newDataStep);
+    step >= lengthSteps && onFinish();
+  }, [step, guideSteps]);
 
-	const handleChangeOptionAction = (keyValue: TYPE_ACTIONS_STEP,newValue: string) => {
-		setOptionSelected({...optionSelected, [keyValue]: newValue})
-		onChangeOption && onChangeOption({...optionSelected, [keyValue]: newValue})
-	}
+  const handleChangeOptionAction = (
+    keyValue: TYPE_ACTIONS_STEP,
+    newValue: string,
+  ) => {
+    setOptionSelected({ ...optionSelected, [keyValue]: newValue });
+    onChangeOption &&
+      onChangeOption({ ...optionSelected, [keyValue]: newValue });
+  };
 
-	const isOptionSelected = (keyValue: TYPE_ACTIONS_STEP, option: string) => {
-		return option === optionSelected[keyValue];
-	}
+  const isOptionSelected = (keyValue: TYPE_ACTIONS_STEP, option: string) => {
+    return option === optionSelected[keyValue];
+  };
 
-	return (
-		<Box style={styles.container} width="100%" p="m">
-			<Card variant="guide" p="s" width="70%">
-				<Text variant="bodyBold" textAlign="center">{title}</Text>
-				{description && <Text variant="bodyRegular" textAlign="center">{description}</Text>}
-			</Card>
-			{
-				action && (
-					<Box top={heightScreen - 370}>
-						<Card my="s" p="xs">
-							<Box>
-								{action.title && (
-									<>
-										<Text textAlign="center">{action.title}</Text>
-										<Divider my="s" />
-									</>
-								)}
-								<Box flexDirection="row" justifyContent="space-around">
-									{action.options.map((option, index) => ( <Button
-										key={`button-option-action-${index}`}
-										m="xs"
-										variant={isOptionSelected(action.key, option.toLowerCase()) ? 'smallMenuActive' : 'smallMenu'}
-										onPress={() => handleChangeOptionAction(action.key, option.toLowerCase())}
-										backgroundColor={isOptionSelected(action.key, option.toLowerCase())  ? 'primaryBlue' : 'white'}>
-										{option}
-									</Button>))}
-								</Box>
-							</Box>
-						</Card>
-					</Box>
-
-				)
-			}
-		</Box>
-
-	)
-}
+  return (
+    <Box style={styles.container} width="100%" p="m">
+      <Card variant="guide" p="s" width="70%">
+        <Text variant="bodyBold" textAlign="center">
+          {title}
+        </Text>
+        {description && (
+          <Text variant="bodyRegular" textAlign="center">
+            {description}
+          </Text>
+        )}
+      </Card>
+      {action && (
+        <Box
+          top={
+            stepBoard === getIndexOfStepForName('tapered')
+              ? 0
+              : heightScreen - 370
+          }>
+          <Card my="s" p="xs">
+            <Box>
+              {action.title && (
+                <>
+                  <Text textAlign="center">{action.title}</Text>
+                  <Divider my="s" />
+                </>
+              )}
+              <Box flexDirection="row" justifyContent="space-around">
+                {action.options.map((option, index) => (
+                  <Button
+                    key={`button-option-action-${index}`}
+                    m="xs"
+                    variant={
+                      isOptionSelected(action.key, option.toLowerCase())
+                        ? 'smallMenuActive'
+                        : 'smallMenu'
+                    }
+                    onPress={() =>
+                      handleChangeOptionAction(action.key, option.toLowerCase())
+                    }
+                    backgroundColor={
+                      isOptionSelected(action.key, option.toLowerCase())
+                        ? 'primaryBlue'
+                        : 'white'
+                    }>
+                    {option}
+                  </Button>
+                ))}
+              </Box>
+            </Box>
+          </Card>
+        </Box>
+      )}
+    </Box>
+  );
+};
 
 const styles = StyleSheet.create({
-	container: {
-		position: 'absolute',
-		backgroundColor: 'transparent',
-		top: -5,
-		left: 0,
-		zIndex: 1,
+  container: {
+    position: 'absolute',
+    backgroundColor: 'transparent',
+    top: -5,
+    left: 0,
+    zIndex: 1,
 
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
-export default GuideStepperBoardComponent
+export default GuideStepperBoardComponent;
