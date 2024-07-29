ca2d35368e824ec90aee69b0dfc01704dd1166c2
Mateo Nieto <matheo.developer@gmail.com>
2024-07-29 17:34:52 -0500

    useGetVersionApp 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@shared/queryClient';
--- src/routes/RootNavigator.tsx
+++ src/routes/RootNavigator.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import { useAppSelector } from '@hooks/useStore';
 import { isAuthenticatedSelector } from '@store/auth/selectors';
+import { useGetVersionApp } from '@hooks/general/useGeneral';
+
 import BaseSpinner from '@ui/components/BaseSpinner';
+import Toast from 'react-native-toast-message';
+import DeviceInfo from 'react-native-device-info';
 
 const PublicNavigator = React.lazy(() => import('./PublicNavigator'));
 const PrivateNavigator = React.lazy(() => import('./PrivateNavigator'));
+
 export const RootNavigator = () => {
   const isAuthenticated = useAppSelector(isAuthenticatedSelector);
+  const { data: versionApp } = useGetVersionApp();
+  const buildNumber = DeviceInfo.getBuildNumber();
+
+  React.useEffect(() => {
+    console.log('=>versionApp[]', versionApp);
+    console.log('=>buildNumber[]', buildNumber);
+
+    if (buildNumber !== versionApp) {
+      Toast.show({
+        type: 'success',
+        text1: 'Please update',
+        text2:
+          'Please update to continue using the app, we have launched new and faster app.',
+      });
+    }
+  }, [buildNumber, versionApp]);
 
   return (
     <React.Suspense fallback={<BaseSpinner />}>
