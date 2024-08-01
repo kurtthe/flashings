4ee4fb0b3b9aa22b01bf8b5fe59da72568bec5a9
Mateo Nieto <matheo.developer@gmail.com>
2024-08-01 17:36:18 -0500

    getting response

--- src/core/hooks/general/useGeneral.ts
+++ src/core/hooks/general/useGeneral.ts
@@ -5,4 +5,6 @@ export const useGetVersionApp = (): UseQueryResult<string, any> =>
   useQuery({
     queryFn: () => getVersionApp(),
     queryKey: ['get_version_app'],
+    refetchInterval: 10000,
+    refetchOnMount: 'always',
   });
