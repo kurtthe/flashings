03a96bdd8e6d6e659f3be6f7f45f2d0710f84a04
Mateo Nieto <matheo.developer@gmail.com>
2024-10-10 15:43:14 -0500

    table landscape

--- src/shared/platform.ts
+++ src/shared/platform.ts
@@ -1,8 +1,18 @@
-import { Platform } from 'react-native';
+import {Platform, Dimensions} from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 
 export const isAndroid = Platform.OS === 'android';
 export const isIOS = Platform.OS === 'ios';
 export default Platform;
+
 export const isTablet = DeviceInfo.isTablet();
 export const hasNotch = DeviceInfo.hasNotch();
+
+let isLandscape =
+  Dimensions.get('window').width > Dimensions.get('window').height;
+
+export const checkIsLandscape = () => isLandscape;
+
+Dimensions.addEventListener('change', ({window}) => {
+  isLandscape = window.width > window.height;
+});
