8070cf6a0824026d77ce798031d3220ce08541bd
Mateo Nieto <matheo.developer@gmail.com>
2024-10-10 19:22:15 -0500

    fix measurement bug when put the same value

--- src/features/flashing/components/Grid/Grid.types.ts
+++ src/features/flashing/components/Grid/Grid.types.ts
@@ -1,4 +1,6 @@
+import {isTablet} from '@shared/platform';
 import {Dimensions} from 'react-native';
+const {width, height} = Dimensions.get('screen');
 
 export const PADDING_BARS = 0;
 export const ALIGN_BARS = 0;
@@ -6,6 +8,6 @@ export const LINE_OFFSET = 0;
 
 export const SIZE_GRID = 50;
 
-export const WIDTH_SCREEN = Dimensions.get('screen').width;
+export const WIDTH_SCREEN = isTablet ? width : height;
 export const HEIGHT_SCREEN = Dimensions.get('screen').height;
 export const COLOR_GRID = '#E1E1E1';
