38302b4063f2fe8965969c2d32bd7a8ea0480bec
Mateo Nieto <matheo.developer@gmail.com>
2024-11-25 17:06:13 +0000

    webview

---
+++ src/components/WebView.tsx
@@ -0,0 +1,44 @@
+import {Box, Text} from '@ui/components';
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {WebView} from 'react-native-webview';
+import type {WebViewProps} from 'react-native-webview';
+import Loading from './Loading';
+
+type Props = WebViewProps & {
+  handlePostMessage: () => void;
+};
+
+const renderErrorWebView = () => (
+  <Box flex={1} alignItems="center">
+    <Text>Not Found...</Text>
+  </Box>
+);
+
+const renderLoading = () => (
+  <Box
+    style={[StyleSheet.absoluteFill]}
+    justifyContent="center"
+    alignItems="center"
+    bg="cards"
+    _dark={{bg: 'backgrounds'}}>
+    <Loading />
+  </Box>
+);
+
+const WebViewComponent = React.forwardRef<WebView, Props>((props, ref) => (
+  <Box flex={1}>
+    <WebView
+      {...props}
+      ref={ref}
+      renderError={renderErrorWebView}
+      renderLoading={renderLoading}
+      onLoadEnd={props.handlePostMessage}
+      incognito={true}
+      cacheEnabled={false}
+      autoManageStatusBarEnabled={false}
+    />
+  </Box>
+));
+
+export default WebViewComponent;
